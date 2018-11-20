5d1143829be677129989c05f3b2bd57f9282ebdd
Muralidhar Chavan <muralidhar.chavan@in.ibm.com>
2018-11-20 16:01:30 +0530

    api key auth

--- src/main/java/com/ibm/service/WatsonServiceManager.java
+++ src/main/java/com/ibm/service/WatsonServiceManager.java
@@ -22,6 +22,7 @@ import com.ibm.watson.developer_cloud.conversation.v1.model.MessageRequest;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
 import com.ibm.watson.developer_cloud.natural_language_classifier.v1.NaturalLanguageClassifier;
 import com.ibm.watson.developer_cloud.natural_language_classifier.v1.model.Classification;
+import com.ibm.watson.developer_cloud.natural_language_classifier.v1.model.ClassifyOptions;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.NaturalLanguageUnderstanding;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalysisResults;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.AnalyzeOptions;
@@ -31,6 +32,7 @@ import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.En
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.Features;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.KeywordsOptions;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.RelationsOptions;
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
 
 /**
  * Integration test for the {@link ConversationService}.
@@ -38,10 +40,10 @@ import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.Re
 public class WatsonServiceManager {
 
   //private static ConversationService service;
-	
+
   private static final String workspaceIdLocal = "13b6d6f7-da64-4af8-8b98-06399daa6f13"; // workbench workspace
-  //private static String workspaceId = "d3a9ca6c-a3b9-4124-83e9-4b1615d3d598";   // Car Dashboard  
-  
+  //private static String workspaceId = "d3a9ca6c-a3b9-4124-83e9-4b1615d3d598";   // Car Dashboard
+
   private static final String usernameConversationLocal = "3a48aa4c-a8e9-4460-b5a2-6ebb81ec742a";
   private static final String passwordConversationLocal =  "X2o8qRqbtIBO";
 
@@ -56,7 +58,7 @@ public class WatsonServiceManager {
   private static Logger logger = Logger.getLogger("workbenchlogger");
 
 
-  
+
   public static String sendMessagesToPredictiveService(String message,PredictiveModel predictiveModel)  {
 	  if (predictiveModel.getServiceName().equals(PredictiveModel.SERVICE_CONVERSATION)) {
 		  return sendMessagesToConversation(message,predictiveModel.getServiceConfig());
@@ -67,7 +69,7 @@ public class WatsonServiceManager {
 	  } else {
 		  return sendMessagesToConversation(message,predictiveModel.getServiceConfig());
 	  }
-  }  
+  }
 
   /**
    * send messages.
@@ -77,27 +79,27 @@ public class WatsonServiceManager {
   public static String sendMessagesToConversation(String message,String config)  {
 
 	System.out.println("MESSAGE :: " + message + " CONFIG >> " + config);
-  
+
 	String workspaceId = System.getenv("CONVERSATION_WORKSPACE_ID"+CONFIG+config);
 	String usernameConversation = System.getenv("CONVERSATION_USERNAME"+CONFIG+config);
 	String passwordConversation =  System.getenv("CONVERSATION_PASSWORD"+CONFIG+config);
 
 	logger.logp(Level.INFO, "WatsonServiceManager", "sendMessagesToConversation", "Bluemix Environment variables:!!! " );
     logger.logp(Level.WARNING, "WatsonServiceManager", "sendMessagesToConversation", "workspaceId =  " + workspaceId );
-    
+
     if (workspaceId == null) {
     	logger.logp(Level.INFO, "WatsonServiceManager", "sendMessagesToConversation", "NON Bluemix Environment variables ??? " );
     	workspaceId = workspaceIdLocal ;
     	usernameConversation = usernameConversationLocal;
     	passwordConversation = passwordConversationLocal ;
     }
-	  
+
 	ConversationService service = new ConversationService(ConversationService.VERSION_DATE_2017_02_03);  //2017-05-26
 	//ConversationService service = new ConversationService("2017-05-26");  //2017-05-26
     service.setUsernameAndPassword(usernameConversation, passwordConversation);
     MessageRequest request = null;
     MessageResponse response = null;
-    		
+
    // request = new MessageRequest.Builder().inputText(message).alternateIntents(true).context(context).build();
     System.out.println("MESSAGE :: " + message);
     request = new MessageRequest.Builder().inputText(message).build();
@@ -118,27 +120,35 @@ public class WatsonServiceManager {
 	String usernameNLC = System.getenv("NLC_USERNAME"+CONFIG+config);
 	String passwordNLC =  System.getenv("NLC_PASSWORD"+CONFIG+config);
 	String classifierIdNLC =  System.getenv("NLC_CLASSIFIER_ID"+CONFIG+config);
+	String apiKey = System.getenv("NLC_API_KEY"+CONFIG+config);
+	String nlcURL = "https://gateway.watsonplatform.net/natural-language-classifier/api";
 
 	logger.logp(Level.INFO, "WatsonServiceManager", "sendMessagesToNLC", "Bluemix Environment variables:!!! " );
     logger.logp(Level.WARNING, "WatsonServiceManager", "sendMessagesToNLC", "workspaceId =  " + classifierIdNLC );
-    
+
     if (classifierIdNLC == null) {
     	logger.logp(Level.INFO, "WatsonServiceManager", "sendMessagesToNLC", "NON Bluemix Environment variables ??? " );
     	usernameNLC = usernameNLCLocal ;
     	passwordNLC = passwordNLCLocal;
     	classifierIdNLC = classifierIdNLCLocal ;
     }
-	
-	NaturalLanguageClassifier service = new NaturalLanguageClassifier();
-	service.setUsernameAndPassword(usernameNLC, passwordNLC);
-	
-	Classification classification = service.classify(classifierIdNLC,message).execute();
+
+    IamOptions options = new IamOptions.Builder().apiKey(apiKey).build();
+
+    NaturalLanguageClassifier service = new NaturalLanguageClassifier(options);
+    service.setEndPoint(nlcURL);
+
+
+	ClassifyOptions classifyOptions = new ClassifyOptions.Builder().classifierId(classifierIdNLC).text(message).build();
+
+
+	Classification classification = service.classify(classifyOptions).execute();
 	//System.out.println(classification);
 
     return classification.toString();
   }
 
-  
+
   /**
    * send messages.
    *
@@ -151,24 +161,24 @@ public class WatsonServiceManager {
 
 		logger.logp(Level.INFO, "WatsonServiceManager", "sendMessagesToNLU", "Bluemix Environment variables:!!! " );
 	    logger.logp(Level.WARNING, "WatsonServiceManager", "sendMessagesToNLU", "usernameNLU =  " + usernameNLU );
-	    
+
 	    if (usernameNLU == null) {
 	    	logger.logp(Level.INFO, "WatsonServiceManager", "sendMessagesToNLU", "NON Bluemix Environment variables ??? " );
 	    	usernameNLU = usernameNLULocal ;
 	    	passwordNLU = passwordNLULocal;
 	    }
-	  
+
 	  NaturalLanguageUnderstanding service = new NaturalLanguageUnderstanding(
 			  NaturalLanguageUnderstanding.VERSION_DATE_2017_02_27,usernameNLU,passwordNLU);
 
 	  /*
 	  String text = "IBM is an American multinational technology " +
-	  
+
 	  "company headquartered in Armonk, New York, " +
 	  "United States, with operations in over 170 countries."; */
-      
+
       //String url = "www.wsj.com/news/markets";
-      
+
 // PARAMETERS
 		EntitiesOptions entitiesOptions = new EntitiesOptions.Builder()
 				  .emotion(true)
@@ -181,16 +191,16 @@ public class WatsonServiceManager {
 		  .sentiment(true)
 		  .limit(30)
 		  .build();
-		
+
 		CategoriesOptions categories = new CategoriesOptions();
-		
+
 		ConceptsOptions concepts= new ConceptsOptions.Builder()
 				  .limit(40)
 				  .build();
-		
+
 		RelationsOptions relations = new RelationsOptions.Builder()
 				  .build();
-		
+
 		Features features = new Features.Builder()
 		  .entities(entitiesOptions)
 		  .keywords(keywordsOptions)
@@ -199,34 +209,34 @@ public class WatsonServiceManager {
 		  .relations(relations)
 		  .build();
 
-		AnalyzeOptions parameters ;			
-		
+		AnalyzeOptions parameters ;
+
 		if (inputType.equalsIgnoreCase(PredictiveModel.INPUT_URL)) {
 			parameters = new AnalyzeOptions.Builder()
 					  .url(message)
 					  .features(features)
-					  .build();			
+					  .build();
 		} else if (inputType.equalsIgnoreCase(PredictiveModel.INPUT_TEXT)) {
 			parameters = new AnalyzeOptions.Builder()
 					  .text(message)
 					  .features(features)
-					  .build();			
+					  .build();
 		} else if (inputType.equalsIgnoreCase(PredictiveModel.INPUT_HTML)) {
 			parameters = new AnalyzeOptions.Builder()
 					  .html(message)
 					  .features(features)
-					  .build();			
-		} else {  
+					  .build();
+		} else {
 			parameters = new AnalyzeOptions.Builder()
 					  .url(message)
 					  .features(features)
-					  .build();			
+					  .build();
 		}
 
-	// PARAMETERS			
+	// PARAMETERS
 	AnalysisResults response = service.analyze(parameters).execute();
 			//System.out.println("NLU Response \n" + response);
-	return response.toString(); 
+	return response.toString();
   }
 
 }
